c519dcb05d283b4884c2b2d5b5bb81e46097537c
Gass-Dev <madebygass@gmail.com>
2021-04-01 02:36:25 +0200

    install VueJs

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # app_ToDo_VueJs
+
+By using vue (without other librairies), build a todo app allowing a user to:
+
+- Display tasks
+- Create a task
+- Edit a task
+- Mark a task as done
+- Delete a task
+- User can see when a task is created, and a task is done
+
+1- Install VueJs
+    -> npm install -g @vue/cli
+2- Create a project
+    -> vue create my-project
+3- Get started:
+    -> cd todo
+    -> npm run serve
---
+++ todo/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Todo App VueJs"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: whitesmoke;
+  margin-top: 60px;
+  background-color: black;
+}
+</style>
